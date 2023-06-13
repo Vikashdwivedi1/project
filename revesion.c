457d49738e8485f29d9e576259dd7414c4fcee0a
Vikash Dwivedi <dwivedivikash530@gmail.com>
2023-06-13 12:14:19 +0530

    second commite

---
+++ revesion.c
@@ -0,0 +1,26 @@
+/*
+  array 
+  pointer
+  recurtion
+  loop
+  -for
+  -while
+  -do while
+  structure
+  {
+    ham idhar struct key word ka use karte hai.
+  }
+  switch 
+  i/o printf(),scanf()
+  string 
+  char variable name[10]="vikash".
+  if -else
+  function ;
+  
+
+
+
+
+
+
+*/
